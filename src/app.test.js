e578814be93df2a12ec9d05ab1dac094ec9be7be
mr-sonalia <sonaliayash@gmail.com>
2022-08-22 22:58:27 +0530

    Files added

---
+++ src/app.test.js
@@ -0,0 +1,32 @@
+const { capitaliseName, multiplyBy2, multiplyBy3, multiplyBy4 } = require("./app");
+
+describe("Sample tests", () => {
+	beforeAll(() => {
+		console.log("Running jest tests");
+	});
+
+	test("Multiply by 2", () => {
+		const result = multiplyBy2(10);
+		expect(result).toBe(20);
+	});
+
+	test("Multiply by 3", () => {
+		const result = multiplyBy3(10);
+		expect(result).toBe(30);
+	});
+
+	test("Multiply by 4", () => {
+		const result = multiplyBy4(10);
+		expect(result).toBe(40);
+	});
+
+	test("Capitalise name", () => {
+		const result = capitaliseName("yash");
+		expect(result).toBe("Yash");
+	});
+
+	afterAll(() => {
+		console.log("Jes tests complete");
+	});
+});
+
